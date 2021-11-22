21dc7cb4be951403fd90a2dad66c8c9293bb0b44
Matthew Ibarra <msibarr@gmail.com>
2021-11-22 13:04:40 -0500

    Adding comments and slight efficiency tweak for copying of arrays during merge step.

--- 01 Divide and Conquer, Sorting and Searching, and Randomized Algorithms/Week 02/Programming Assignment/Number of Inversions/Java/src/NumberOfInversions.java
+++ 01 Divide and Conquer, Sorting and Searching, and Randomized Algorithms/Week 02/Programming Assignment/Number of Inversions/Java/src/NumberOfInversions.java
@@ -15,47 +15,58 @@ public class NumberOfInversions {
     }
 
     public static long mergeSortHelper(int[] nums, int[] aux, int left, int right) {
-        long numInversions = 0;
-        int mid;
-
         // Base case
-        if (right > left) {
-            // Calculate the mid to divide the array into two parts and loop.
-            mid = (left + right) / 2;
-            long leftInversions = mergeSortHelper(nums, aux, left, mid);
-            long rightInversions = mergeSortHelper(nums, aux, mid + 1, right);
-
-            numInversions = leftInversions + rightInversions + merge(nums, aux, left, mid + 1, right);
+        if (right <= left) {
+            return 0;
         }
-        return numInversions;
+
+        // Calculate the mid to divide the array into two parts and loop.
+        // Divide
+        int mid = (left + right) / 2;
+        // Conquer
+        long leftInversions = mergeSortHelper(nums, aux, left, mid);
+        long rightInversions = mergeSortHelper(nums, aux, mid + 1, right);
+
+        // Merge and add up inversion counts
+        return leftInversions + rightInversions + merge(nums, aux, left, mid + 1, right);
+        // T(n) = 2T(n/2) + O(n)
+        // a = 2, b = 2, d = 1
+        // According to Master Method, the running time complexity is O(n log n).
     }
 
     public static int merge(int[] nums, int[] aux, int left, int mid, int right) {
         int leftPtr = left, midPtr = mid, auxPtr = left, inversionsCount = 0;
 
+        // Work through each array and place into sorted order in  aux array while counting inversions.
         while ((leftPtr <= mid - 1) && (midPtr <= right)) {
+            // if left value <= right value.
             if (nums[leftPtr] <= nums[midPtr]) {
+                // Just put left value into aux array.
                 aux[auxPtr++] = nums[leftPtr++];
             } else {
+                // If right > left, put right value into aux array.
                 aux[auxPtr++] = nums[midPtr++];
-
+                // And count all values from mid to current left value in left array as inversions.
                 inversionsCount += (mid - leftPtr);
             }
         }
 
+        // Once we've reached the end of one of the two arrays, add all remaining values to the aux array.
+        // Do this for the left array.
         while (leftPtr <= mid - 1) {
             aux[auxPtr++] = nums[leftPtr++];
         }
 
+        // Do thsi for the right array.
         while (midPtr <= right) {
             aux[auxPtr++] = nums[midPtr++];
         }
 
-        for (leftPtr = left; leftPtr <= right; leftPtr++) {
-            nums[leftPtr] = aux[leftPtr];
-        }
+        // Copy newly ordered values from aux array into nums array.
+        System.arraycopy(aux, left, nums, left, right - left + 1);
 
         return inversionsCount;
+        // Running time complexity: O(n)
     }
 
     // Driver method to test the above function
